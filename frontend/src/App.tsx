389863a47591f727753cac9b6a3043ba8226945a
Giovero <giovanni.vero@gmail.com>
2023-10-14 11:20:56 +0200

    fe-add icon and change font to open sans

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 function App() {
-  return <div className="text-xl text-black bg-red-500">Hello world</div>;
+  return (
+    <div className="text-xl text-black bg-red-500">
+      Hello world
+      <div>
+        <BeakerIcon className="h-6 w-6 text-black" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
